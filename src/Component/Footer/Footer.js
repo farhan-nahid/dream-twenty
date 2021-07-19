2756e9d3d78d19512336455ac457dd22cbcb49e2
farhan-nahid <nahidahmed9933@gmail.com>
2021-07-19 18:18:22 +0600

    add something

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <footer className="text-center mt-5">
-            <small >©2021. <a target="-blank" href="https://www.facebook.com/farhan.nahid.01">Farhan Ahmed Nahid</a>. All rights reserved. Dhaka, Bangladesh.</small>
-        </footer>
-    );
+  return (
+    <footer className="text-center mt-5">
+      <small>
+        ©2021.{" "}
+        <a target="-blank" href="https://www.facebook.com/dev.farhanNahid">
+          Farhan Ahmed Nahid
+        </a>
+        . All rights reserved. Dhaka, Bangladesh.
+      </small>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,23 +1,26 @@
-import { faUsers } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
-import './Header.css';
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light mb-5">
-             <div className="container">
-                <a className="navbar-brand fw-bolder fst-italic" href="/">
-                    Dream <span className="text-danger">Twenty</span>
-                </a>
-                    <div>
-                    <button className=" fw-bolder fst-italic btn btn-danger">
-                        Log In <span className="ms-2"><FontAwesomeIcon icon={faUsers} /></span>
-                    </button>
-                    </div>
-                 </div>
-          </nav>
-    );
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light mb-5">
+      <div className="container">
+        <a className="navbar-brand fw-bolder fst-italic" href="/">
+          Dream <span className="text-danger">Twenty</span>
+        </a>
+        <div>
+          <button className=" fw-bolder fst-italic btn btn-danger">
+            Log In{" "}
+            <span className="ms-2">
+              <FontAwesomeIcon icon={faUsers} />
+            </span>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
